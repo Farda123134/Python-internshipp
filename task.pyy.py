aa758c599789d289f51af9c24d1ade29560b405b
Farda Noor <ah0448097@gmail.com>
2025-06-24 01:38:24 -0700

    Add files via upload

---
+++ task.pyy.py
@@ -0,0 +1,10 @@
+# 1. Take inputs from user
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float number: "))
+string_input = input("Enter a string: ")
+
+# 2. Display using formatted print
+print(f"\nYou entered:")
+print(f"Integer: {int_input}")
+print(f"Float: {float_input}")
+print(f"String: {string_input}")
