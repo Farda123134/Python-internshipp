3363c8c36f9be785df2fea863a8f82e0e12965c9
Farda Noor <ah0448097@gmail.com>
2025-06-26 01:50:10 -0700

    Add files via upload
    his Python program takes a sentence from the user and:
    
    Splits it into a list of words using split()
    
    Prints the list and then joins it back using " - " with join()
    
    Creates a tuple for the user's first and last name
    
    Prints each name using indexing
    
    It demonstrates working with lists, split/join functions, and tuples in Python.

---
+++ task 3.pyy.py
@@ -0,0 +1,22 @@
+# 🔹 Step 1: Take sentence input from user
+sentence= input("Enter sentence:")
+
+# 🔹 Step 2: Split the sentence into a list of words
+words_list= sentence.split()
+
+# 🔹 Step 3: Print the list of words
+print('List of words:', words_list)
+
+# 🔹 Step 4: Join the list back into a sentence with ' - ' between words
+joined_sentence= "-".join(words_list)
+print("Joined with dashes:", joined_sentence)
+# 🔹 Step 5: Create a tuple with first and last name
+
+my_name= ("Farda","Noor" )
+
+
+
+# 🔹 Step 6: Print each part using indexing
+print("First Name:", my_name[0])
+print("Second Name:",my_name[1] )
+
